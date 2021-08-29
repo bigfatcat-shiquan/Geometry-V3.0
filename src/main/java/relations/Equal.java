9c9be2d60442d9b2e34ae81b45bf6a572b15919c
bigfatcat-shiquan <501346999@qq.com>
2021-08-29 16:40:55 +0800

    2021年8月29日结构功能定义层开发完成

---
+++ src/main/java/relations/Equal.java
@@ -0,0 +1,116 @@
+package relations;
+
+import elements.Element;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * 类：Equal 相等关系式
+ * 意义即为多个元素之间的相等关系，写作equal(元素1, 元素2)，如边AB=边AC，则写作equal(segment("A", "B"), segment("A", "C"))
+ * 也可以写作equal(HashSet<元素>)，其中HashSet中元素个数至少有2个，来表示这一个集合中的元素均是相等的
+ * 在欧式几何中，很多要证明的问题和定理均可以转化为相等关系式，也就是equal对象的形式来描述
+ * */
+public class Equal {
+
+    /**定义全局所出现的所有相等关系式的带索引集合*/
+    private final static HashMap<String, Equal> global_all_equals = new HashMap<>();
+
+    /**定义相等关系式的唯一字符串编码*/
+    private final String geometry_key;
+
+    /**定义相等关系式的内容属性：相等的这组元素的集合*/
+    private final HashSet<Element> units_set;
+
+    /**定义生成相等关系式的唯一字符串编码的方式*/
+    public static String makeGeometry_key(HashSet<Element> this_units_set) {
+        StringBuilder this_geometry_key_builder = new StringBuilder();
+        ArrayList<String> units_key_list = new ArrayList<>();
+        for (Element this_unit : this_units_set) {
+                units_key_list.add(this_unit.getGeometry_key());
+        }
+        units_key_list.sort(Comparator.naturalOrder());
+        for (String this_unit_key : units_key_list) {
+            this_geometry_key_builder.append(this_unit_key);
+            this_geometry_key_builder.append("=");
+        }
+        String this_geometry_key = this_geometry_key_builder.toString();
+        return this_geometry_key.substring(0, this_geometry_key.length()-1);
+    }
+
+    /**
+     * 定义获取一个相等关系式对象的代码表达式
+     * 两种方式：equal(元素1, 元素2)，元素1和元素2不能本来就相同 或 equal(HashSet<元素>)，HashSet内元素个数必须大于等于2
+     * */
+    public static Equal equal(Element unit_1, Element unit_2) throws Exception{
+        HashSet<Element> this_units_set = new HashSet<>();
+        if (unit_1.equals(unit_2)) {
+            throw new Exception("两个本来就相同的元素不能生成equal对象！");
+        }
+        this_units_set.add(unit_1);
+        this_units_set.add(unit_2);
+        String this_geometry_key = makeGeometry_key(this_units_set);
+        if (global_all_equals.containsKey(this_geometry_key)) {
+            return global_all_equals.get(this_geometry_key);
+        } else {
+            return new Equal(this_geometry_key, this_units_set);
+        }
+    }
+
+    public static Equal equal(HashSet<Element> this_units_set) throws Exception{
+        if (this_units_set.size() < 2) {
+            throw new Exception("组成相等关系式的元素个数不能少于2个！");
+        }
+        String this_geometry_key = makeGeometry_key(this_units_set);
+        if (global_all_equals.containsKey(this_geometry_key)) {
+            return global_all_equals.get(this_geometry_key);
+        } else {
+            return new Equal(this_geometry_key, this_units_set);
+        }
+    }
+
+    /**生成一个相等关系式对象的构造函数*/
+    private Equal(String this_geometry_key, HashSet<Element> this_units_set) {
+        this.geometry_key = this_geometry_key;
+        this.units_set = this_units_set;
+        global_all_equals.put(this.geometry_key, this);
+    }
+
+    /**获取一个相等关系式对象的唯一字符串编码*/
+    public String getGeometry_key() {
+        return this.geometry_key;
+    }
+
+    /**获取一个相等关系式的内容属性：相等的这组元素的集合*/
+    public HashSet<Element> getUnits_set() {
+        return this.units_set;
+    }
+
+    /**判断相等关系式所含元素是否被另一个相等关系式所完全包含，如AB=AC已经被AB=AC=CD所完全包含*/
+    public Boolean isContainedOf(Equal another_equal){
+        return another_equal.units_set.containsAll(this.units_set);
+    }
+
+    /**获取一个相等关系式的集合除去某个元素或另一个相等关系式集合之后的集合部分*/
+    public HashSet<Element> getExceptSetOf(Element another_unit) {
+        HashSet<Element> result_units_set = new HashSet<>(this.units_set);
+        result_units_set.remove(another_unit);
+        return result_units_set;
+    }
+
+    public HashSet<Element> getExceptSetOf(Equal another_equal) {
+        HashSet<Element> result_units_set = new HashSet<>(this.units_set);
+        result_units_set.removeAll(another_equal.units_set);
+        return result_units_set;
+    }
+
+    /**获取一个相等关系式的集合和另一个相等关系式集合的交集部分*/
+    public HashSet<Element> getInnerSetOf(Equal another_equal) {
+        HashSet<Element> result_units_set = new HashSet<>(this.units_set);
+        result_units_set.retainAll(another_equal.units_set);
+        return result_units_set;
+    }
+
+}
---
+++ src/test/java/Geometry3Test.java
@@ -0,0 +1,225 @@
+import elements.*;
+import relations.Equal;
+
+public class Geometry3Test {
+
+    public static void main(String[] args) throws Exception {
+
+//        测试用例1，验证边、加和、乘积元素的使用正确
+        Segment x1 = Segment.segment("A", "B");
+        Segment x2 = Segment.segment("A", "C");
+        Segment x3 = Segment.segment("C", "A");
+
+        SumUnits z1 = SumUnits.sumUnits(SumUnits.sumUnits(x1, x3), SumUnits.sumUnits(x2, x3));
+        SumUnits z2 = SumUnits.sumUnits(z1, z1);
+        SumUnits z3 = SumUnits.sumUnits(SumUnits.sumUnits(x1, z1), SumUnits.sumUnits(x2, SumUnits.sumUnits(x3, x2)));
+        MultiplyUnits z4 = MultiplyUnits.multiplyUnits(z3, x3);
+        MultiplyUnits z5 = MultiplyUnits.multiplyUnits(x2, z2);
+        MultiplyUnits z6 = MultiplyUnits.multiplyUnits(z4, z3);
+        MultiplyUnits z7 = MultiplyUnits.multiplyUnits(z5, z2);
+
+        System.out.print(z1.getGeometry_key() + "\n");
+        System.out.print(z1.getUnit_counts() + "\n");
+        System.out.print(z1.getSubUnits() + "\n");
+        System.out.print(z3.getGeometry_key() + "\n");
+        System.out.print(z3.getUnit_counts() + "\n");
+        System.out.print(z3.getSubUnits() + "\n");
+        System.out.print(z2.equals(z3) + "\n");
+        System.out.print(x1 + "\n");
+        System.out.print(x2 + "\n");
+        System.out.print(x3 + "\n");
+        System.out.print(z6.getGeometry_key() + "\n");
+        System.out.print(z6.getUnit_counts() + "\n");
+        System.out.print(z6.getSubUnits() + "\n");
+        System.out.print(z7.getGeometry_key() + "\n");
+        System.out.print(z7.getUnit_counts() + "\n");
+        System.out.print(z7.getSubUnits() + "\n");
+        System.out.print(z6.equals(z7) + "\n");
+        System.out.print(z4 + "\n");
+        System.out.print(z5 + "\n");
+        System.out.print("\n");
+//        3AC+AB
+//        {elements.Segment@677327b6=3, elements.Segment@1540e19d=1}
+//        [elements.Segment@677327b6, elements.Segment@1540e19d]
+//        2AB+6AC
+//        {elements.Segment@677327b6=6, elements.Segment@1540e19d=2}
+//        [elements.Segment@677327b6, elements.Segment@1540e19d]
+//        true
+//        elements.Segment@1540e19d
+//        elements.Segment@677327b6
+//        elements.Segment@677327b6
+//        (2AB+6AC)^2*AC
+//        {elements.Segment@677327b6=1, elements.SumUnits@14ae5a5=2}
+//        [elements.Segment@677327b6, elements.SumUnits@14ae5a5]
+//        (2AB+6AC)^2*AC
+//        {elements.Segment@677327b6=1, elements.SumUnits@14ae5a5=2}
+//        [elements.Segment@677327b6, elements.SumUnits@14ae5a5]
+//        true
+//        elements.MultiplyUnits@7f31245a
+//        elements.MultiplyUnits@7f31245a
+
+//        测试用例2，验证角、角度、加和元素的使用正确
+        Angle a1 = Angle.angle("C", "B", "A");
+        Angle a2 = Angle.angle("D", "B", "A");
+        Angle a3 = Angle.angle("A", "B", "D");
+        Degree a4 = Degree.degree(180);
+        Degree a5 = Degree.degree(180);
+
+        SumUnits s1 = SumUnits.sumUnits(SumUnits.sumUnits(a1, a2), SumUnits.sumUnits(a2, a3));
+        SumUnits s2 = SumUnits.sumUnits(s1, s1);
+        SumUnits s3 = SumUnits.sumUnits(SumUnits.sumUnits(a1, s1), SumUnits.sumUnits(a2, SumUnits.sumUnits(a3, a2)));
+        SumUnits s4 = SumUnits.sumUnits(s2, a5);
+        SumUnits s5 = SumUnits.sumUnits(s3, a4);
+
+        System.out.print(s1.getGeometry_key() + "\n");
+        System.out.print(s1.getUnit_counts() + "\n");
+        System.out.print(s1.getSubUnits() + "\n");
+        System.out.print(s3.getGeometry_key() + "\n");
+        System.out.print(s3.getUnit_counts() + "\n");
+        System.out.print(s3.getSubUnits() + "\n");
+        System.out.print(s2.equals(s3) + "\n");
+        System.out.print(a1 + "\n");
+        System.out.print(a2 + "\n");
+        System.out.print(a3 + "\n");
+        System.out.print(a4 + "\n");
+        System.out.print(a5 + "\n");
+        System.out.print(a2.equals(a3) + "\n");
+        System.out.print(a4.equals(a5) + "\n");
+        System.out.print(s4.getGeometry_key() + "\n");
+        System.out.print(s4.getUnit_counts() + "\n");
+        System.out.print(s4.getSubUnits() + "\n");
+        System.out.print(s5.getGeometry_key() + "\n");
+        System.out.print(s5.getUnit_counts() + "\n");
+        System.out.print(s5.getSubUnits() + "\n");
+        System.out.print(s4.equals(s5) + "\n");
+        System.out.print("\n");
+//        3∠ABD+∠ABC
+//        {elements.Angle@6d6f6e28=1, elements.Angle@135fbaa4=3}
+//        [elements.Angle@6d6f6e28, elements.Angle@135fbaa4]
+//        2∠ABC+6∠ABD
+//        {elements.Angle@6d6f6e28=2, elements.Angle@135fbaa4=6}
+//        [elements.Angle@6d6f6e28, elements.Angle@135fbaa4]
+//        true
+//        elements.Angle@6d6f6e28
+//        elements.Angle@135fbaa4
+//        elements.Angle@135fbaa4
+//        elements.Degree@45ee12a7
+//        elements.Degree@45ee12a7
+//        true
+//        true
+//        180°+2∠ABC+6∠ABD
+//        {elements.Degree@45ee12a7=1, elements.Angle@6d6f6e28=2, elements.Angle@135fbaa4=6}
+//        [elements.Degree@45ee12a7, elements.Angle@6d6f6e28, elements.Angle@135fbaa4]
+//        180°+2∠ABC+6∠ABD
+//        {elements.Degree@45ee12a7=1, elements.Angle@6d6f6e28=2, elements.Angle@135fbaa4=6}
+//        [elements.Degree@45ee12a7, elements.Angle@6d6f6e28, elements.Angle@135fbaa4]
+//        true
+
+//        测试用例3，验证元素类附属方法的正确
+        System.out.print(x1.isComplex() + "\n");
+        System.out.print(z1.isComplex() + "\n");
+        System.out.print(x1.getLength() + "\n");
+        System.out.print(z1.getLength() + "\n");
+        System.out.print(z3.getLength() + "\n");
+        System.out.print(z4.getLength() + "\n");
+        System.out.print(z7.getLength() + "\n");
+        System.out.print(x1.getPoints() + "\n");
+        System.out.print(a1.getVertex() + "\n");
+        System.out.print(a1.getAside_points() + "\n");
+        System.out.print(a4.getValue() + "\n");
+        System.out.print("\n");
+//        false
+//        true
+//        1
+//        4
+//        8
+//        2
+//        3
+//        [A, B]
+//        B
+//        [A, C]
+//        180
+
+//        测试用例4，验证复合元素类相关附属方法的正确
+        System.out.print(x1.isSubOf(z1) + "\n");
+        System.out.print(z1.isSubOf(z2) + "\n");
+        System.out.print(z2.isSubOf(z4) + "\n");
+        System.out.print(a1.isSubOf(s5) + "\n");
+        System.out.print(s3.isSubOf(s5) + "\n");
+        System.out.print(a5.isSubOf(s5) + "\n");
+        System.out.print(z4.isSameClassOf(z5) + "\n");
+
+        System.out.print(a5.isSubOf(a5) + "\n");
+        System.out.print(x1.isSubOf(x2) + "\n");
+        System.out.print(z2.isSubOf(z1) + "\n");
+        System.out.print(z2.isSubOf(z3) + "\n");
+        System.out.print(z1.isSubOf(z4) + "\n");
+        System.out.print(a5.isSubOf(s3) + "\n");
+        System.out.print("\n");
+//        7*true
+//        6*false
+        x1 = Segment.segment("A", "B");
+        x2 = Segment.segment("C", "B");
+        x3 = Segment.segment("C", "D");
+        Segment x4 = Segment.segment("D", "C");
+        s1 = SumUnits.sumUnits(SumUnits.sumUnits(x3, x4), SumUnits.sumUnits(x1, x2));
+        s2 = SumUnits.sumUnits(x1, SumUnits.sumUnits(x3, x4));
+        s3 = SumUnits.sumUnits(x1, x2);
+        System.out.print(s2.getInnerOf(s3).getGeometry_key() + "\n");
+        System.out.print(s1.getInnerOf(s2).getGeometry_key() + "\n");
+        System.out.print(s1.getInnerOf(s1).getGeometry_key() + "\n");
+        System.out.print(s1.getInnerOf(s5) + "\n");
+        System.out.print(s1.getInnerOf(s2).equals(s2.getInnerOf(s1)) + "\n");
+        System.out.print(s1.getExceptOf(s3).getGeometry_key() + "\n");
+        System.out.print(s1.getExceptOf(s2).getGeometry_key() + "\n");
+        System.out.print(s1.getExceptOf(x1).getGeometry_key() + "\n");
+        System.out.print(s1.getExceptOf(x3).getGeometry_key() + "\n");
+        System.out.print(s1.getExceptOf(s2).equals(x2) + "\n");
+        System.out.print(s1.getExceptOf(x3).equals(s1.getExceptOf(x4)) + "\n");
+        System.out.print("\n");
+//        AB
+//        2CD+AB
+//        2CD+AB+BC
+//        null
+//        true
+//        2CD
+//        BC
+//        2CD+BC
+//        AB+BC+CD
+//        true
+//        true
+
+//        测试用例5，验证等量关系类相关附属方法的正确
+        Equal e1 = Equal.equal(a1, a2);
+        Equal e2 = Equal.equal(a1, a4);
+        Equal e3 = Equal.equal(a1, a5);
+        System.out.print(e1.getGeometry_key() + "\n");
+        System.out.print(e2.getGeometry_key() + "\n");
+        System.out.print(e3.getGeometry_key() + "\n");
+        System.out.print(e1.getUnits_set() + "\n");
+        System.out.print(e2.getUnits_set() + "\n");
+        System.out.print(e3.getUnits_set() + "\n");
+        System.out.print(e1.getExceptSetOf(a1) + "\n");
+        System.out.print(e1.getExceptSetOf(e2) + "\n");
+        System.out.print(e1.getInnerSetOf(e2) + "\n");
+        System.out.print(e2.equals(e3) + "\n");
+        System.out.print(e2.getInnerSetOf(e3) + "\n");
+        System.out.print(e2.getExceptSetOf(e3) + "\n");
+//        ∠ABC=∠ABD
+//        180°=∠ABC
+//        180°=∠ABC
+//        [elements.Angle@6d6f6e28, elements.Angle@135fbaa4]
+//        [elements.Angle@6d6f6e28, elements.Degree@45ee12a7]
+//        [elements.Angle@6d6f6e28, elements.Degree@45ee12a7]
+//        [elements.Angle@135fbaa4]
+//        [elements.Angle@135fbaa4]
+//        [elements.Angle@6d6f6e28]
+//        true
+//        [elements.Angle@6d6f6e28, elements.Degree@45ee12a7]
+//        []
+
+
+
+    }
+
+}
